40017383b817e00e17964e2d0b1847fa5aa01de3
BigBoyz2314 <zazimran@gmail.com>
2023-12-16 17:53:31 +0500

    changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function initMultiStepForm() {
     function topFunction() {
       document.body.scrollTop = 0; // For Safari
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-      window.parent.scrollTo(0,0);
+      window.parent.postMessage('scrollToTop', 'https://freeafrann.com/');
     }
 
     if (progressNumber !== stepsNumber) {
