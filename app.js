81cdf2487178c7aaedebd57973d72a7b94a84903
BigBoyz2314 <zazimran@gmail.com>
2023-12-16 17:46:54 +0500

    change

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ function initMultiStepForm() {
     function topFunction() {
       document.body.scrollTop = 0; // For Safari
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      window.parent.scrollTo(0,0);
     }
 
     if (progressNumber !== stepsNumber) {
